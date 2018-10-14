b62abc5db92ed3b7788690d523a1e3ef4d706e4c
Thomas Edwards <tomo12321@hotmail.com>
2018-10-14 16:13:06 +1300

    added main menu

---
+++ parrychris_OneHit_P2/Assets/Scripts/MainMenu/TomsButtonHandler.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TomsButtonHandler : MonoBehaviour {
+
+	private GameObject MainMenu;
+	private GameObject HowToPlay;
+
+	void Start(){
+		MainMenu = GameObject.Find("MenuPanel");
+		HowToPlay = GameObject.Find("HowToPlayPanel");
+		if(MainMenu ==null || HowToPlay==null){
+			Debug.Log("Error finding game objects of name MenuPanel and HowToPlayPanel");
+		}
+	}
+
+	public void playMap1(){
+		SceneManager.LoadScene("Map1");
+	}
+	public void playMap2(){
+		SceneManager.LoadScene("Map2");
+	}
+
+	public void Quit()
+	{
+		//If we are running in a standalone build of the game
+	#if UNITY_STANDALONE
+		//Quit the application
+		Application.Quit();
+	#endif
+
+		//If we are running in the editor
+	#if UNITY_EDITOR
+		//Stop playing the scene
+		UnityEditor.EditorApplication.isPlaying = false;
+	#endif
+	}
+
+	public void ToMainMenuFromHowToPlay(){
+		HowToPlay.SetActive(false);
+		MainMenu.SetActive(true);
+	}
+
+	public void ToHowToPlayFromMainMenu(){
+		MainMenu.SetActive(false);
+		HowToPlay.SetActive(true);
+		
+	}
+}
